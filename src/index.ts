41f92a16dae2e2c5dc16c0be496e844857c4a66c
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2025-08-27 20:55:18 +0200

    Major feature update

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
     return res.status(200).json({
-        message: "Hey there"
+        message: "Hey there, we have succesfully complete the CD integrations"
     })
 })
 
